86b787a4bcda69abb61e85766a9443e140790dc4
HoangTrung2004 <lenguyenhoangtrung2004@gmail.com>
2025-10-02 10:40:32 +0700

    add PS43701_Slide4

---
+++ PS43701_Silde4/src/main/java/web/ps43701_silde4/Ps43701Silde4Application.java
@@ -0,0 +1,13 @@
+package web.ps43701_silde4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ps43701Silde4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ps43701Silde4Application.class, args);
+	}
+
+}
---
+++ PS43701_Silde4/src/test/java/web/ps43701_silde4/Ps43701Silde4ApplicationTests.java
@@ -0,0 +1,13 @@
+package web.ps43701_silde4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ps43701Silde4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
